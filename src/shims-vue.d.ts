59735852a28eca58f72209a25b4536598bd4fc08
everettmorgan <admin@ejmorgan.com>
2021-12-05 23:38:07 -0800

    consolidate shims

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,33 @@
 /* eslint-disable */
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+  import type {DefineComponent} from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+declare module '*.css' {
+  const classes: { [key: string]: string };
+  export = classes;
+}
+
+/* eslint-disable */
+
+import Vue, {VNode} from "vue";
+
+declare global {
+  namespace JSX {
+    interface Element extends VNode {
+    }
+
+    interface ElementClass extends Vue {
+    }
+
+    interface ElementAttributesProperty {
+      $props: {};
+    }
+
+    interface IntrinsicElements {
+      [elemName: string]: any;
+    }
+  }
 }
